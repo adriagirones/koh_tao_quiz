036c62ec4cdd58a55c01c5006dcf3ae7eaccfab0
stevenhiltrop <stevenhiltrop@gmail.com>
2020-08-12 16:17:49 +0700

    Turned on debug

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$=0b%7xa7=j!6j@94*j1=ogx$basuf8&l6aro&t-@xr4w2^*6$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
