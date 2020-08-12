820f4b7210fdf3791915fa50848e38ef95a31b0b
Adrià Gironès <girones94@gmail.com>
2020-08-12 14:43:17 +0700

    Added was_published_recently() in manage.py and test.py

--- quiz/polls/tests.py
+++ quiz/polls/tests.py
@@ -16,4 +16,12 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days=1)
         future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        self.assertIs(future_question.was_published_today(), False)
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
